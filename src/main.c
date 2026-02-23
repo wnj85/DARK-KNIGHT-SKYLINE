42a47ecce137968a3f1e232ef84133bd28cc309d
wnj85 <wnj.joyce@gmail.com>
2026-02-23 20:27:41 +0000

    Refactor time and date handling in main.c

--- src/main.c
+++ src/main.c
@@ -1,119 +1,138 @@
 #include <pebble.h>
-  
+
+#define BAT_SIGNAL_DURATION 3000  // ms
+
 static Window *s_main_window;
+static BitmapLayer *s_gotham_layer;
+static BitmapLayer *s_batsignal_layer;
 static TextLayer *s_time_layer;
-static TextLayer *s_date_layer;
 
-static BitmapLayer *s_background_layer;
-static GBitmap *s_background_bitmap;
+static GBitmap *s_gotham_bitmap;
+static GBitmap *s_batsignal_bitmap;
 
+static bool s_showing_batsignal = false;
+static AppTimer *s_batsignal_timer = NULL;
+
+static void hide_batsignal_callback(void *data);
 
 static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
-
-  // Create a long-lived buffer
-  static char time_buffer[] = "00:00";
-  static char date_buffer[] = "Xxxxxxxx 00 Xxx";
-
-  // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-    //Use 24h hour format
-    strftime(time_buffer, sizeof("00:00"), "%H:%M", tick_time);
-  } else {
-    //Use 12 hour format
-    strftime(time_buffer, sizeof("00:00"), "%I:%M", tick_time);
+  if (s_showing_batsignal) {
+    return;
   }
-  
-  // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, time_buffer);
-  
-  // Write the current date into the buffer
-  strftime(date_buffer, sizeof("Xxxxxxxx 00 Xxx"), "%A %e %b", tick_time);
-  
-  // Display this on the TextLayer
-  text_layer_set_text(s_date_layer, date_buffer);
+
+  static char s_buffer[8];
+
+  time_t temp = time(NULL);
+  struct tm *t = localtime(&temp);
+
+  if (clock_is_24h_style()) {
+    strftime(s_buffer, sizeof(s_buffer), "%H:%M", t);
+  } else {
+    strftime(s_buffer, sizeof(s_buffer), "%I:%M", t);
+  }
+
+  text_layer_set_text(s_time_layer, s_buffer);
 }
 
-
-static void main_window_load(Window *window) {
-  // Create GBitmap, then set to created BitmapLayer
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND_COL);
-  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
-  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
-  
-  // Create Time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 25, 144, 50));
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorWhite);
-  text_layer_set_text(s_time_layer, "00:00");
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  
-  // Create Date TextLayer and add to Window
-  s_date_layer = text_layer_create(GRect(0, 144, 144, 20));
-  text_layer_set_background_color(s_date_layer, GColorClear);
-  text_layer_set_text_color(s_date_layer, GColorWhite);
-  text_layer_set_text(s_date_layer, "Xxxxxxxx 00 Xxx");
-  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
-  
-  // Make sure the time is displayed from the start
+static void hide_batsignal_callback(void *data) {
+  s_showing_batsignal = false;
+  layer_set_hidden(bitmap_layer_get_layer(s_batsignal_layer), true);
+  layer_set_hidden(text_layer_get_layer(s_time_layer), false);
   update_time();
 }
 
+static void accel_tap_handler(AccelAxisType axis, int32_t direction) {
+  s_showing_batsignal = true;
 
-static void main_window_unload(Window *window) {
-  //Destroy GBitmap
-  gbitmap_destroy(s_background_bitmap);
+  layer_set_hidden(text_layer_get_layer(s_time_layer), true);
+  layer_set_hidden(bitmap_layer_get_layer(s_batsignal_layer), false);
 
-  //Destroy BitmapLayer
-  bitmap_layer_destroy(s_background_layer);
-  
-  // Destroy Time TextLayer
-  text_layer_destroy(s_time_layer);
-  
-  // Destroy Date TextLayer
-  text_layer_destroy(s_date_layer);
+  if (s_batsignal_timer) {
+    app_timer_cancel(s_batsignal_timer);
+    s_batsignal_timer = NULL;
+  }
+
+  s_batsignal_timer = app_timer_register(BAT_SIGNAL_DURATION, hide_batsignal_callback, NULL);
 }
-
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
-  
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  // Background color for night sky (dark blue)
+  window_set_background_color(window, GColorOxfordBlue);
+
+  // Gotham skyline along the bottom
+  s_gotham_bitmap = gbitmap_create_with_resource(BG_Buildings.png);
+  s_gotham_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_gotham_layer, s_gotham_bitmap);
+  bitmap_layer_set_compositing_mode(s_gotham_layer, GCompOpSet);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_gotham_layer));
+
+  // Time in the night sky (top)
+  GRect time_frame = GRect(0, 10, bounds.size.w, 50);
+  s_time_layer = text_layer_create(time_frame);
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
+  // Bat Signal bitmap in the sky area
+  s_batsignal_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BAT_SIGNAL);
+  GRect bat_bounds = gbitmap_get_bounds(s_batsignal_bitmap);
+  int16_t bat_x = (bounds.size.w - bat_bounds.size.w) / 2;
+  int16_t bat_y = 5;
+  GRect bat_frame = GRect(bat_x, bat_y, bat_bounds.size.w, bat_bounds.size.h);
+
+  s_batsignal_layer = bitmap_layer_create(bat_frame);
+  bitmap_layer_set_bitmap(s_batsignal_layer, s_batsignal_bitmap);
+  bitmap_layer_set_compositing_mode(s_batsignal_layer, GCompOpSet);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_batsignal_layer));
+
+  layer_set_hidden(bitmap_layer_get_layer(s_batsignal_layer), true);
+
+  update_time();
+}
+
+static void main_window_unload(Window *window) {
+  if (s_batsignal_timer) {
+    app_timer_cancel(s_batsignal_timer);
+    s_batsignal_timer = NULL;
+  }
+
+  text_layer_destroy(s_time_layer);
+
+  bitmap_layer_destroy(s_gotham_layer);
+  gbitmap_destroy(s_gotham_bitmap);
+
+  bitmap_layer_destroy(s_batsignal_layer);
+  gbitmap_destroy(s_batsignal_bitmap);
+}
 
 static void init() {
-  // Create main Window element and assign to pointer
   s_main_window = window_create();
-
-  // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
-
-  // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
-  
-  // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-}
 
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  accel_tap_service_subscribe(accel_tap_handler);
+
+  update_time();
+}
 
 static void deinit() {
-  // Destroy Window
+  accel_tap_service_unsubscribe();
+  tick_timer_service_unsubscribe();
   window_destroy(s_main_window);
 }
-
 
 int main(void) {
   init();
